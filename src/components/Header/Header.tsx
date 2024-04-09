ac3f54512ebe30975c544465fdf42d39108c90d3
Arijit Kundu <arikundu9@gmail.com>
2024-04-10 02:31:13 +0530

    about 1/2 done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,18 +16,24 @@ export default function Header() {
     ];
     return (
         <header>
-            <div className="container mx-auto flex flex-wrap justify-between p-5">
+            <div className="boredr border-b-2 border-fuchsia-600/20">
+                <div className="container mx-auto flex justify-end">
+                    <div className="p-3 text-lg font-semibold">+91 1234567890</div>
+                    <div className="p-3 text-lg font-semibold">email@domain.com</div>
+                </div>
+            </div>
+            <div className="container mx-auto flex flex-wrap md:justify-between justify-center p-5">
                 <Link href="/" className="flex title-font font-medium items-center text-gray-900 mb-4">
-                    <img src="/logo.png" alt="logo" width="200px" height="auto"/>
+                    <img src="/logo.png" alt="logo" width="200px" height="auto" />
                     {/* <div className="logo_text">
                         <span className="text-[#fe5855]">Back</span>
                         <span className="text-[#6721ff]">Link</span>Studio.com
                     </div> */}
                 </Link>
-                <div className="flex flex-wrap items-center text-base justify-center">
+                <div className="flex flex-nowrap items-center text-base justify-center">
                     {NavLinks.map((nav, i) => {
                         return (
-                            <Link key={i} href={nav.path} className={`m-5 ${pathName === nav.path ? "text-[#6721ff]" : "text-white"} text-xl font-semibold hover:text-[#6721ff]`}>
+                            <Link key={i} href={nav.path} className={`m-5 text-nowrap ${pathName === nav.path ? "text-[#6721ff]" : "text-white"} text-xl font-semibold hover:text-[#6721ff]`}>
                                 {nav.name}
                             </Link>
                         );
